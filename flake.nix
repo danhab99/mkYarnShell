3f630c2b8c2711e3892e89ac3520a898b87341cf
Dan Habot <dan.habot@gmail.com>
2025-09-28 15:44:06 +0000

    reworking flake to be reusable

--- flake.nix
+++ flake.nix
@@ -1,100 +1,58 @@
 {
+  description = "Flake exporting mkNodeModulesShell and mkYarnNodeModules";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem flake-utils.lib.defaultSystems (system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-        lib = pkgs.lib;
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
 
         readNamePackageJson = { packageJsonPath }:
           let
-            # Read the file contents
             packageJsonContents = builtins.readFile packageJsonPath;
-
-            # Parse the JSON
             packageData = builtins.fromJSON packageJsonContents;
-
-            # Extract the name
-            packageName = packageData.name;
           in
-            packageName;
+            packageData.name;
 
-        mkYarnShell = yarnShellInputs@{
-          src
-          ,node_modules_name ? "default"
-          ,...
-        }: let
-          name = readNamePackageJson {
-            packageJsonPath = src + "/package.json";
+        mkNodeModulesShell = { src, node_modules_name ? "default", ... }@args:
+          let
+            name = readNamePackageJson {
+              packageJsonPath = src + "/package.json";
+            };
+          in
+            pkgs.mkShell (args // {
+              shellHook = ''
+                flake_path=".#node_modules.$(uname -m)-linux.${node_modules_name}"
+                nix build $flake_path --no-link
+                echo $flake_path
+                node_path=$(nix path-info $flake_path)
+                echo $node_path 
+                rm -rf ./node_modules
+                ln -s $node_path/libexec/${name}/node_modules ./node_modules
+
+                ${args.shellHook or ""}
+              '';
+            });
+
+        mkYarnNodeModules = { name, src }:
+          let
+            package = pkgs.mkYarnPackage { inherit name src; };
+          in {
+            node_modules_path = "${package}/libexec/${name}/node_modules";
           };
-
-        in pkgs.mkShell (yarnShellInputs // {
-          shellHook = ''
-              flake_path=".#node_modules.$(uname -m)-linux.${node_modules_name}"
-              nix build $flake_path --no-link
-              echo $flake_path
-              node_path=$(nix path-info $flake_path)
-              echo $node_path 
-              rm -rf ./node_modules
-              ln -s $node_path/libexec/${name}/node_modules ./node_modules
-
-              ${yarnShellInputs.shellHook}
-          '';
-        });
 
       in {
-        # packages.node_modules.default = pkgs.mkYarnPackage {
-        node_modules.default = pkgs.mkYarnPackage {
-          name = "yarn-project";
-          src = ./.;
+        # Ready-to-use default dev shell
+        # devShells.default = mkNodeModulesShell { src = ./.; };
+
+        # Export builder functions for reuse (system-dependent)
+        lib = {
+          inherit mkNodeModulesShell mkYarnNodeModules readNamePackageJson;
         };
-
-        node_modules.project1 = pkgs.mkYarnPackage {
-          name = "project1";
-          src = ./project1;
-        };
-
-        devShells = {
-          default = mkYarnShell {
-            src = ./.;
-
-            packages = with pkgs; [
-              gnumake
-              nodejs_22
-              yarn 
-              prettierd
-            ];
-
-            shellHook = ''
-              zsh
-            '';
-          };
-
-
-          project1 = mkYarnShell {
-            src = ./project1;
-            node_modules_name = "project1";
-
-            packages = with pkgs; [
-              gnumake
-              nodejs_22
-              yarn 
-              prettierd
-            ];
-
-            shellHook = ''
-              zsh
-            '';
-          };
-        };
-      });
+      }
+    );
 }
-
-
